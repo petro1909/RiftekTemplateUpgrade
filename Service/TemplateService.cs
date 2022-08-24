1ebf8be0862d9e3a481605de5f8210db74b4de0a
petro1909 <petya-97_97@mail.ru>
2022-08-24 16:17:57 +0300

    Add template parsing methods

--- Service/TemplateService.cs
+++ Service/TemplateService.cs
@@ -5,16 +5,26 @@ namespace RiftekTemplateUpgrade.Service
 {
     public class TemplateService
     {
+
+        public List<Template> Templates;
+        public TemplateService()
+        {
+            Templates = new List<Template>();
+        }
+
         public List<Template> GetTemplatesFromJson(string json)
         {
 
             List<Template> templates = JsonConvert.DeserializeObject<List<Template>>(json);
+            Templates = templates;
             return templates;
         }
 
         public string WriteTemplatesToJson(List<Template> templates)
         {
-            return JsonConvert.SerializeObject(templates);
+            JsonSerializerSettings settings = new JsonSerializerSettings();
+            settings.Formatting = Formatting.Indented;
+            return JsonConvert.SerializeObject(templates, settings);
         } 
     }
 }
