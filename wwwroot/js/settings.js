a2d86f146f7e6a622ea4f947ce984c46a83ec334
petro1909 <petya-97_97@mail.ru>
2022-09-06 21:19:16 +0300

    replace js functions from html file to separate files and put them in js folder

---
+++ wwwroot/js/settings.js
@@ -0,0 +1,33 @@
+﻿
+async function LoadDefaultCommParamsForm() {
+    const response = await fetch("/connection/GetDefaultConnectionParameters", {
+        method: "GET",
+        headers: { "Accept": "application/json" }
+    });
+    if (response.ok) {
+        connectionSettings = await response.json();
+        $("#desktop_ip").val(connectionSettings.desktopAddress);
+        $("#desktop_port").val(connectionSettings.desktopPort);
+        $("#scanner_ip").val(connectionSettings.scannerAddress);
+    }
+}
+LoadDefaultCommParamsForm();
+
+async function SaveCommSettingsForm() {
+
+    connectionSettings.desktopAddress = $("#desktop_ip").val();
+    connectionSettings.desktopPort = $("#desktop_port").val();
+    connectionSettings.scannerAddress = $("#scanner_ip").val();
+
+    console.log("ewfewf")
+    const response = await fetch("/connection/SetConnectionParameters", {
+        method: "POST",
+        headers: {
+            'Content-Type': 'application/json;charset=utf-8'
+        },
+        body: JSON.stringify(connectionSettings)
+    });
+    if (response.ok) {
+
+    }
+}
---
+++ wwwroot/js/templates.js
@@ -0,0 +1,124 @@
+﻿var templates = []
+var connectionSettings;
+
+let settings = document.getElementById("template_settings");
+
+
+//Получение списка шаблонов из json файла
+async function GetTemplates() {
+    const response = await fetch("/template/templates", {
+        method: "GET",
+        headers: { "Accept": "application/json" }
+    });
+    if (response.ok) {
+        templates = await response.json();
+        console.log(templates)
+        templates.forEach(template => {
+            CreateTempateDiv(template);
+        });
+        CreateAddTemplateDiv()
+    }
+}
+GetTemplates();
+
+//Создание кнопки для каждого шаблона
+function CreateTempateDiv(template) {
+    let button = document.createElement("button");
+    button.textContent = template.number;
+    button.className = "template_item";
+    button.onclick = function () {
+        //Изменеие стиля нажатой кнопки
+        $(".template_item").removeClass("clicked");
+        $(this).addClass("clicked")
+        //Вывод данных шаблона
+        ShowSettings(template);
+    }
+    $('#templates_data').append(button);
+}
+
+//Создание кнопки для добавления шаблона
+function CreateAddTemplateDiv() {
+    let button = document.createElement("button");
+    button.textContent = "Add";
+    button.className = "template_item";
+    button.onclick = function () {
+        //Изменеие стиля нажатой кнопки
+        $(".template_item").removeClass("clicked");
+        $(this).addClass("clicked")
+        //Вывод данных шаблона
+        AddTemplate();
+    }
+    $('#templates_data').append(button);
+}
+
+
+
+async function AddTemplate() {
+    const response = await fetch("/template/AddTemplate", {
+        method: "GET",
+        headers: { "Accept": "application/json" }
+    });
+    if (response.ok) {
+        $("#templates_data").empty();
+        GetTemplates();
+    }
+}
+
+async function DeleteTemplate(template) {
+    $.ajax({
+        type: "GET",                                       
+        url: "/template/DeleteTemplate",
+        data: { number: template.number },
+        success: function () {
+            window.location.reload()
+        }
+    });
+}
+
+
+
+//Вывод данных шаблона
+function ShowSettings(template) {
+    for (const [key, val] of Object.entries(template.scannerSettings)) {
+        $('#' + key).text(val)
+    }
+
+    $("#save_settings").removeAttr("disabled");
+    $("#save_settings").unbind();
+    $("#save_settings").bind('click', function () {
+        console.log("fefef")
+        SaveCurrentSettingsToTemplate(template);
+    });
+
+    $("#delete_template").removeAttr("disabled");
+    $("#delete_template").unbind();
+    $('#delete_template').bind('click', function () {
+        console.log("dwdw")
+        DeleteTemplate(template);
+    });
+}
+
+
+
+async function SaveCurrentSettingsToTemplate(template) {
+    console.log(JSON.stringify(template))
+    const response = await fetch("/scanner/GetScannerSettings", {
+        method: "GET",
+        headers: { "Accept": "application/json" }
+    });
+    if (response.ok) {
+        template.scannerSettings = await response.json();
+    }
+
+    const response1 = await fetch("/template/SaveTempalteSettings", {
+        method: "POST",
+        headers: {
+            'Content-Type': 'application/json;charset=utf-8'
+        },
+        body: JSON.stringify(template)
+    });
+    if (response1.ok) {
+        ShowSettings(template);
+    }
+}
+
